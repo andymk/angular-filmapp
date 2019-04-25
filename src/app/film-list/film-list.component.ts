83bdae3539dbfcce6ccadcac055f452f013579b4
Andy Knight <andym.knight@gmail.com>
2019-04-25 12:12:26 +0100

    basic app layout

---
+++ src/app/film-list/film-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-film-list",
+  templateUrl: "./film-list.component.html",
+  styleUrls: ["./film-list.component.css"]
+})
+export class FilmListComponent implements OnInit {
+  pageTitle: string = "Film List";
+  constructor() {}
+
+  ngOnInit() {}
+}
